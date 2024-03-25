3d0ccade116dffbaefe624adf79960d5bd1118a0
ranjanxsumit <98314060+ranjanxsumit@users.noreply.github.com>
2024-03-25 11:34:54 +0530

    Add files via upload

---
+++ ChildMind Monitor Forum/client/src/components/common/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">Community Forum </h1>
+      <p class="lead">Your Partner in Children’s Mental Health Journey !</p>
+      <hr class="my-4" />
+      <p className="lead">A disscussion forum for enthusiastic children.</p>
+    </div>
+  );
+};
+
+export default Jumotron;
